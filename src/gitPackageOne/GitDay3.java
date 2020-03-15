24ca780ce74e8b340d27e1502b20e58d2af5373f
abdurrahim <abdurrahim.ozturk93@gmail.com>
2020-03-15 21:41:41 +0100

    third commit from local branch

--- src/gitPackageOne/GitDay3.java
+++ src/gitPackageOne/GitDay3.java
@@ -12,6 +12,7 @@ public class GitDay3 {
 		System.out.println("My Firts changes on local master");
 		System.out.println("This is my feature branch");
 		System.out.println("my second push from local feature branch");
+		System.out.println("my third push from local feature branch");
 		
 		
 	}
